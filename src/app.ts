dea14858e8c44945531490c20c544b2da348d8a4
José Rocha <joserochadev@gmail.com>
2024-04-15 16:02:07 -0300

    feat: add create attendee route to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import { createEvent } from './routes/createEvent'
+import { createAttendee } from './routes/createAttendee'
 
 export const app = fastify()
 
 app.register(createEvent)
+app.register(createAttendee)
