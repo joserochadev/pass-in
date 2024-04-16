6049e34d440322fb7077b0b050799ed1e38c3139
José Rocha <joserochadev@gmail.com>
2024-04-16 11:44:26 -0300

    feat: create getEvent method

--- src/repositories/IEventRepository.ts
+++ src/repositories/IEventRepository.ts
@@ -2,4 +2,5 @@ import { IEvent } from '../interfaces/IEvent'
 
 export interface IEventRepository {
 	create(event: Omit<IEvent, 'id'>): Promise<IEvent>
+	getEvent(eventId: string): Promise<IEvent | null>
 }
